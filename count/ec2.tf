c39506c987ea9f955cc8d03525ce6850e2453326
Shirishapathulothu <pathulothushirisha@gmail.com>
2025-08-04 17:59:41 +0530

    terraform

---
+++ count/ec2.tf
@@ -0,0 +1,47 @@
+resource "aws_security_group" "allow_terraform" {
+  name        = "allow_terraform"
+  description = "Allow port number 22 to access ssh"
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+   tags = merge(
+        var.common_tags,
+        {
+            Name = "allow_terraform"
+        }
+    )
+}
+
+resource "aws_instance" "terraform" {
+    count = length(var.instance_names)
+    ami = "ami-09c813fb71547fc4f"
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [aws_security_group.allow_terraform.id]
+    # tags = {
+    #     Name = "terraform"
+    # }
+    tags = merge(
+        var.common_tags,
+        {
+            Name = var.instance_names[count.index]
+        }
+    )
+}
+
+
+
+
